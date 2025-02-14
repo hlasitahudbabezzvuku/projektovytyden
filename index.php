403c86c164f8ae212220c3ba6e8ed4ca19751996
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-14 11:02:51 +0100

    database connection

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
+require('Medoo.php');
+use Medoo\Medoo;
 
+$database = new Medoo([
+	'type' => 'mysql',
+	'host' => 'localhost',
+	'database' => 'name',
+	'username' => 'your_username',
+	'password' => 'your_password',
+ 
+	'command' => [
+		'SET SQL_MODE=ANSI_QUOTES'
+	]
+])
 
 ?>
