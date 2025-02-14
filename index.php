78eb0e3a972eeba9cec5a0808a24b84e50b35138
Samuel Huf <samhuf@seznam.cz>
2025-02-14 11:49:48 +0100

    database host

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Medoo\Medoo;
 
 $database = new Medoo([
 	'type' => 'mysql',
-	'host' => 'localhost',
+	'host' => 'sql105.infinityfree.com',
 	'database' => 'if0_38314982_projektovy_tyden',
 	'username' => 'if0_38314982',
 	'password' => 'ojc0EveY78Zip',
