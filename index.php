b673cbe3ab269fd454fc78ca9770dedb9c25014b
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-17 19:29:34 +0100

    add header

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
     ?>
   </head>
   <body>
+    <h1>PubZ</h1>
     <span><a href="login.php?mode=join">Připojit se</a></span>
     <span><a href="login.php?mode=host">Vytvořit</a></span>
     <span><a href="login.php?mode=single">Hrát sám</a></span>
