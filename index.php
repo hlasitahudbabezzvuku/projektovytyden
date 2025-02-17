4ca0e7262b1355e195f4581fa540d501c039ca9e
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-17 08:26:06 +0100

    add basic html structure

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
-<?php
+<!DOCTYPE php>
+<html lang="cz">
+  <head>
+    <meta charset="UTF-8">
+    <title>PubZ</title>
+    <?php
 
-require('Medoo.php');
-use Medoo\Medoo;
+    require('Medoo.php');
+    use Medoo\Medoo;
 
-$database = new Medoo([
-	'type' => 'mysql',
-	'host' => 'sql105.infinityfree.com',
-	'database' => 'if0_38314982_projektovy_tyden',
-	'username' => 'if0_38314982',
-	'password' => 'ojc0EveY78Zip',
-	'command' => [
-		'SET SQL_MODE=ANSI_QUOTES'
-	]
-]);
+    $database = new Medoo([
+    'type' => 'mysql',
+    'host' => 'sql105.infinityfree.com',
+    'database' => 'if0_38314982_projektovy_tyden',
+    'username' => 'if0_38314982',
+    'password' => 'ojc0EveY78Zip',
+    'command' => [
+    'SET SQL_MODE=ANSI_QUOTES'
+    ]
+    ]);
 
-echo("Nevim");
+    echo("Nevim");
+    ?>
 
-?>
+  </head>
+  <body>
+
+  </body>
+</html>
+
