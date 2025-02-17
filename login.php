0f2a69591986cf34b9a29559153151158b87a327
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-17 08:50:35 +0100

    add login.php

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE php>
+<html lang="cz">
+  <head>
+    <meta charset="UTF-8">
+    <title>PubZ</title>
+    <?php
+
+    session_start();
+
+    require('Medoo.php');
+    use Medoo\Medoo;
+
+    if (!array_key_exists('database', $_SESSION)) {
+      $_SESSION["database"] = new Medoo([
+        'type' => 'mysql',
+        'host' => 'sql105.infinityfree.com',
+        'database' => 'if0_38314982_projektovy_tyden',
+        'username' => 'if0_38314982',
+        'password' => 'ojc0EveY78Zip',
+        'command' => [
+        'SET SQL_MODE=ANSI_QUOTES'
+        ]
+      ]);
+    }
+
+    ?>
+  </head>
+  <body>
+  </body>
+</html>
+
