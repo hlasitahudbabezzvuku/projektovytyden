c65f169cb9cbad9a81bf215664a5880655692564
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-17 08:46:25 +0100

    add check for database connection

--- connect.php
+++ connect.php
@@ -10,16 +10,18 @@
     require('Medoo.php');
     use Medoo\Medoo;
 
-    $_SESSION["database"] = new Medoo([
-    'type' => 'mysql',
-    'host' => 'sql105.infinityfree.com',
-    'database' => 'if0_38314982_projektovy_tyden',
-    'username' => 'if0_38314982',
-    'password' => 'ojc0EveY78Zip',
-    'command' => [
-    'SET SQL_MODE=ANSI_QUOTES'
-    ]
-    ]);
+    if (!array_key_exists('database', $_SESSION)) {
+      $_SESSION["database"] = new Medoo([
+        'type' => 'mysql',
+        'host' => 'sql105.infinityfree.com',
+        'database' => 'if0_38314982_projektovy_tyden',
+        'username' => 'if0_38314982',
+        'password' => 'ojc0EveY78Zip',
+        'command' => [
+        'SET SQL_MODE=ANSI_QUOTES'
+        ]
+      ]);
+    }
 
     ?>
   </head>
--- index.php
+++ index.php
@@ -10,18 +10,18 @@
     require('Medoo.php');
     use Medoo\Medoo;
 
-    $_SESSION["database"] = new Medoo([
-    'type' => 'mysql',
-    'host' => 'sql105.infinityfree.com',
-    'database' => 'if0_38314982_projektovy_tyden',
-    'username' => 'if0_38314982',
-    'password' => 'ojc0EveY78Zip',
-    'command' => [
-    'SET SQL_MODE=ANSI_QUOTES'
-    ]
-    ]);
-
-    echo("Nevim");
+    if (!array_key_exists('database', $_SESSION)) {
+      $_SESSION["database"] = new Medoo([
+        'type' => 'mysql',
+        'host' => 'sql105.infinityfree.com',
+        'database' => 'if0_38314982_projektovy_tyden',
+        'username' => 'if0_38314982',
+        'password' => 'ojc0EveY78Zip',
+        'command' => [
+        'SET SQL_MODE=ANSI_QUOTES'
+        ]
+      ]);
+    }
 
     ?>
   </head>
