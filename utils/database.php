90c5ddf7d25a518f0bb45fc840cf48c328401328
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-18 22:14:21 +0100

    new structure

---
+++ utils/database.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "Medoo.php";
+use Medoo\Medoo;
+
+$database = new Medoo([
+  "type" => "mysql",
+  "host" => "sql105.infinityfree.com",
+  "database" => "if0_38314982_projektovy_tyden",
+  "username" => "if0_38314982",
+  "password" => "ojc0EveY78Zip",
+  "command" => [
+    "SET SQL_MODE=ANSI_QUOTES"
+  ]
+]);
+
+?>
