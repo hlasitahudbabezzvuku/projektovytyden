229a2f861b882b98beefcaf6bc5d39945489e8a4
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-18 09:22:32 +0100

    fix game code hanling by removing size parameter

--- login.php
+++ login.php
@@ -33,7 +33,7 @@
       $code = rand(100000, 999999);
 
       echo(
-        "<img src=\"https://api.qrserver.com/v1/create-qr-code/?data=" . urlencode("http://pubz.infinityfreeapp.com/login.php?game=" . $code) . ";size=400x400\" alt=\"qr-code\"/>" .
+        "<img src=\"https://api.qrserver.com/v1/create-qr-code/?data=" . urlencode("http://pubz.infinityfreeapp.com/login.php?game=" . $code) . "\" alt=\"qr-code\"/>" .
         "<form action=\"monitor.php?id=" . $code . "\" method=\"GET\">" .
         number_format($code, 0, '.', ' ')
       );
