bd3ed8142fea11b91ef7e5644fa5878e4fe9d19e
Samuel Huf <samhuf@seznam.cz>
2025-02-18 08:38:05 +0100

    novy soubor pro pripojeni databaze

--- login.php
+++ login.php
@@ -8,19 +8,11 @@
     session_start();
 
     require('Medoo.php');
+    require_once 'database.php';
     use Medoo\Medoo;
 
     if (!array_key_exists('database', $_SESSION)) {
-      $_SESSION["database"] = new Medoo([
-        'type' => 'mysql',
-        'host' => 'sql105.infinityfree.com',
-        'database' => 'if0_38314982_projektovy_tyden',
-        'username' => 'if0_38314982',
-        'password' => 'ojc0EveY78Zip',
-        'command' => [
-        'SET SQL_MODE=ANSI_QUOTES'
-        ]
-      ]);
+      $_SESSION["database"] = $database;
     }
 
     ?>
