a79f2d6dc0b38ec3be25ab0d7bd51ddd3818defd
Samuel Huf <samhuf@seznam.cz>
2025-02-18 11:39:56 +0100

    Pridavani otazek na POST endpointu add-questions.php

--- add-questions.php
+++ add-questions.php
@@ -1,10 +1,74 @@
 <?php
-    require_once 'database.php';
+require_once 'database.php';
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $otazky = $_POST["otazky"];
-        print_r($otazky);
+function generateUUID() {
+    $data = random_bytes(16);
+
+    // Set version (4) in the correct position
+    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+    // Set the variant to RFC 4122
+    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+    return $data; // Returns binary(16) directly
+}
+
+function pridatOdpovedi($odpovedi) {
+    global $databaseLocal;
+    $odpovedi_id = generateUUID();
+
+    while ($databaseLocal->get('Odpovedi', '*', ['id' => $odpovedi_id])) {
+        $odpovedi_id = generateUUID();
     }
+
+    $databaseLocal->insert('Odpovedi', [
+        'id' => $odpovedi_id,
+        'a' => $odpovedi['a'],
+        'b' => $odpovedi['b'],
+        'c' => $odpovedi['c'],
+        'd' => $odpovedi['d'],
+        'spravna' => $odpovedi['spravna']
+    ]);
+
+    return $odpovedi_id;
+}
+
+function pridatOtazku($otazka, $typ) {
+    global $databaseLocal;
+    $otazka_id = generateUUID();
+    while ($databaseLocal->get($typ.'Otazky', '*', ['id' => $otazka_id,])) {
+        $otazka_id = generateUUID();
+    }
+    $databaseLocal->insert($typ.'Otazky', [
+        'id' => $otazka_id,
+        $typ => $otazka[$typ],
+        'odpovedi' => pridatOdpovedi($otazka['odpovedi'])
+    ]);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $json_data = file_get_contents('php://input');
+    $data = json_decode($json_data, true);
+    $textove = $data['textove'];
+    $zvuk = $data['zvuk'];
+    $video = $data['video'];
+    $ilustrace = $data['ilustrace'];
+
+    foreach ($textove as $otazka) {
+        pridatOtazku($otazka, "text");
+    }
+
+    foreach ($zvuk as $otazka) {
+        pridatOtazku($otazka, "zvuk");
+    }
+
+    foreach ($video as $otazka) {
+        pridatOtazku($otazka, "video");
+    }
+
+    foreach ($ilustrace as $otazka) {
+        pridatOtazku($otazka, "ilustrace");
+    }
+}
 ?>
 
 <!doctype html>
@@ -17,6 +81,6 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Pridani otazek</p>
+<p>Pridani otazek</p>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,86 +1,25 @@
 <?php
-require_once 'database.php';
 
-function generateUUID() {
-    $data = random_bytes(16);
 
-    // Set version (4) in the correct position
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
-    // Set the variant to RFC 4122
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
-
-    return $data; // Returns binary(16) directly
+if (isset($_SESSION) || session_status() !== PHP_SESSION_NONE) {
+  session_destroy();
 }
 
-function pridatOdpovedi($odpovedi) {
-    global $databaseLocal;
-    $odpovedi_id = generateUUID();
+require "database.php";
 
-    while ($databaseLocal->get('Odpovedi', '*', ['id' => $odpovedi_id])) {
-        $odpovedi_id = generateUUID();
-    }
-
-    $databaseLocal->insert('Odpovedi', [
-        'id' => $odpovedi_id,
-        'a' => $odpovedi['a'],
-        'b' => $odpovedi['b'],
-        'c' => $odpovedi['c'],
-        'd' => $odpovedi['d'],
-        'spravna' => $odpovedi['spravna']
-    ]);
-
-    return $odpovedi_id;
-}
-
-function pridatOtazku($otazka, $typ) {
-    global $databaseLocal;
-    $otazka_id = generateUUID();
-    while ($databaseLocal->get($typ.'Otazky', '*', ['id' => $otazka_id,])) {
-        $otazka_id = generateUUID();
-    }
-    $databaseLocal->insert($typ.'Otazky', [
-        'id' => $otazka_id,
-        $typ => $otazka[$typ],
-        'odpovedi' => pridatOdpovedi($otazka['odpovedi'])
-    ]);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $json_data = file_get_contents('php://input');
-    $data = json_decode($json_data, true);
-    $textove = $data['textove'];
-    $zvuk = $data['zvuk'];
-    $video = $data['video'];
-    $ilustrace = $data['ilustrace'];
-
-    foreach ($textove as $otazka) {
-        pridatOtazku($otazka, "text");
-    }
-
-    foreach ($zvuk as $otazka) {
-        pridatOtazku($otazka, "zvuk");
-    }
-
-    foreach ($video as $otazka) {
-        pridatOtazku($otazka, "video");
-    }
-
-    foreach ($ilustrace as $otazka) {
-        pridatOtazku($otazka, "ilustrace");
-    }
-}
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
+<!DOCTYPE html>
+<html lang="cz">
+  <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<p>Pridani otazek</p>
-</body>
+    <title>PubZ</title>
+  </head>
+  <body>
+    <h1>PubZ</h1>
+    <span><a href="login.php?mode=join">Připojit se</a></span>
+    <span><a href="login.php?mode=host">Vytvořit</a></span>
+    <span><a href="login.php?mode=single">Hrát sám</a></span>
+  </body>
 </html>
+
