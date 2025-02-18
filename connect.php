a980a599f9885fa62d1b1c088694ef36ae3d4c44
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-18 12:38:22 +0100

    swap code and name error checking order in connect.php

--- connect.php
+++ connect.php
@@ -7,16 +7,16 @@ if (isset($_SESSION) || session_status() !== PHP_SESSION_NONE) {
 session_start();
 require("database.php");
 
-if (array_key_exists("name", $_GET)) {
-  $_SESSION["name"] = $_GET["name"];
-  header("Location: http://pubz.infinityfreeapp.com/login.php?game=" . $_GET["code"]);
-  die();
-}
-
 if (array_key_exists("code", $_GET)) {
   $_SESSION["code"] = $_GET["code"];
 } else {
   header("Location: http://pubz.infinityfreeapp.com/login.php");
+  die();
+}
+
+if (array_key_exists("name", $_GET)) {
+  $_SESSION["name"] = $_GET["name"];
+  header("Location: http://pubz.infinityfreeapp.com/login.php?game=" . $_GET["code"]);
   die();
 }
 
