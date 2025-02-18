7c65981e12fc0986c7a274af2def3ebffd8218cd
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-18 22:55:05 +0100

    make get_players actually do something

--- api/get_players.php
+++ api/get_players.php
@@ -3,6 +3,12 @@
 require $_SERVER['DOCUMENT_ROOT'] . "/utils/database.php";
 global $database;
 
-echo "<span style=\"color: #0000ff;\">player</span>"
+$data = $database->select("Players", "name", [
+	"game" => $_GET["game"]
+]);
+
+foreach($data as $item) {
+	echo $item["name"] . "<br>";
+}
 
 ?>
