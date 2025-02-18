956515f5ef1fb8fb74b2dda6e4562b19b537af8c
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-18 12:45:08 +0100

    add secondary condition to creadentioals autentification

--- connect.php
+++ connect.php
@@ -7,14 +7,14 @@ if (isset($_SESSION) || session_status() !== PHP_SESSION_NONE) {
 session_start();
 require("database.php");
 
-if (array_key_exists("code", $_GET)) {
+if (array_key_exists("code", $_GET) && $$_GET["code"] != "") {
   $_SESSION["code"] = $_GET["code"];
 } else {
   header("Location: http://pubz.infinityfreeapp.com/login.php?failed=true");
   die();
 }
 
-if (array_key_exists("name", $_GET)) {
+if (array_key_exists("name", $_GET) && $$_GET["name"] != "") {
   $_SESSION["name"] = $_GET["name"];
 } else {
   header("Location: http://pubz.infinityfreeapp.com/login.php?falied=true;game=" . $_GET["code"]);
