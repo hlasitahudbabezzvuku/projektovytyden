2c3d2f473b5dbd7938bfddf421d9792ee069cb70
Samuel Huf <samhuf@seznam.cz>
2025-02-18 16:59:42 +0100

    Opravovani pridavani pres javascript

--- add-questions.php
+++ add-questions.php
@@ -72,12 +72,9 @@ if ($_POST) {
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="cz">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
