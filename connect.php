3e49dbdacc3cae17ce2c4c5e00c6ac3bcd891c23
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-19 11:04:23 +0100

    fix http request

--- connect.php
+++ connect.php
@@ -50,7 +50,7 @@ if (count($data) != 0) {
   <body>
     <span>Vyčkejte na začátek hry.</span>
     <script>
-    function get_stage() { fetch('http://pubz.infinityfreeapp.com/api/get-stage.php?game=" . $code . "')
+    function get_stage() { fetch('http://pubz.infinityfreeapp.com/api/get-stage.php?game=' + <?php $code ?>)
       .then(function (response) { return response.text(); })
       .then(function (text) {
         console.log(text);
