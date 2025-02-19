d6f138bf0934e0d43e1f320f1cffe92bfa98002f
Samuel Huf <samhuf@seznam.cz>
2025-02-19 09:30:46 +0100

    poijaksdsjkjaps

--- api/get-questions.php
+++ api/get-questions.php
@@ -9,7 +9,30 @@
     $gameCode = $_SESSION["code"];
 
     $currentStage = $database->get("Games",'stage');
-    $textOtazky = $database->select("textOtazky", ["[>]Odpovedi"=>["id_odpovedi" => "id"]], ["text", "a", "b", "c", "d", "spravna"]);
+    $typ = "";
+
+    if ($currentStage < 0 || $currentStage > 8) {
+        header("Location: http://pubz.infinityfreeapp.com/login.php?failed=" . urlencode("Tvoje hra je v divnem stavu."));
+        die();
+    }
+
+    switch ($currentStage) {
+        case 1:
+            $typ = "text";
+            break;
+        case 3:
+            $typ = "video";
+            break;
+        case 5:
+            $typ = "zvuk";
+            break;
+        case 7:
+            $typ = "ilustrace";
+            break;
+    }
+
+
+    $textOtazky = $database->select($typ."Otazky", ["[>]Odpovedi"=>["id_odpovedi" => "id"]], '*');
     
     for ($i = 0; $i < count($textOtazky); $i++) {
         $textOtazky[$i]["id"] = bin2hex($textOtazky[$i]["id"]);
@@ -17,4 +40,7 @@
     }
     $jsonOtazky = json_encode($textOtazky);
     print_r($currentStage);
+    print_r($textOtazky);
+
+    exit();
 ?>
