afd0724c34cdebd1f89f4c5c4086e9edc45f7a2e
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-19 08:47:04 +0100

    add end.php

---
+++ end.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!isset($_SESSION) || session_status() === PHP_SESSION_NONE) {
+  header("Location: http://pubz.infinityfreeapp.com/login.php?failed=" . urlencode("Houstone, máme problém! :/"));
+  die();
+}
+
+session_start();
+require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
+require $_SERVER["DOCUMENT_ROOT"] . "/utils/uuid.php";
+global $database;
+
+if (!array_key_exists("code", $_SESSION) || empty($_SESSION["code"])) {
+  header("Location: http://pubz.infinityfreeapp.com/login.php?failed=" . urlencode("Nastala vyjímka, neočekávaná softwarová vyjímka... :("));
+  die();
+}
+
+if (!array_key_exists("name", $_SESSION) || empty($_SESSION["name"])) {
+  header("Location: http://pubz.infinityfreeapp.com/login.php?failed=" . urlencode("Zažiješ Ameriku! :(") . "&game=" . $_GET["code"]);
+  die();
+}
+
+$data = $database->select("Games", ["id"], ["id" => $_SESSION["code"]]);
+
+if (count($data) == 0) {
+  header("Location: http://pubz.infinityfreeapp.com/login.php?failed=" . urlencode("A chčije a chčije... :("));
+  die();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="cz">
+  <head>
+    <meta charset="UTF-8">
+    <title>PubZ</title>
+  </head>
+  <body>
+  </body>
+</html>
+
