edb866f5b8b1d6b51f25b3abb78b0049d9306e5e
Samuel Huf <samhuf@seznam.cz>
2025-02-19 23:57:08 +0100

    sadjfapsoiu

--- api/generate-questions.php
+++ api/generate-questions.php
@@ -2,21 +2,46 @@
   require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
   global $database;
 
-    $otazky = $database->select("Otazky", ["id"], ["type" => $typ]);
-    print_r($otazky);
-    $keys = array_rand($otazky, 3);
-    print_r($keys);
+  $currentStage = $database->get("Games", 'stage', [
+    "id" => $gameCode
+  ]);
+  $typ = "";
 
-    $database->delete("GamesOtazky", ["game_id" => $gameCode]);
+  if ($currentStage < 0 || $currentStage > 8) {
+    header("Location: http://pubz.infinityfreeapp.com/index.php?failed=" . urlencode("Tvoje hra je v divnem stavu."));
+    die();
+  }
 
-    $order = 0;
-    foreach ($keys as $key) {
-        $database->insert("GamesOtazky", [
-            "game_id" => $_GET['code'],
-            "otazka_id" => $otazky[$key]["id"],
-            "position" => $order,
-        ]);
-        $order++;
-      }
-    exit();
+  switch ($currentStage) {
+    case 1:
+        $typ = "text";
+        break;
+    case 3:
+        $typ = "video";
+        break;
+    case 5:
+        $typ = "zvuk";
+        break;
+    case 7:
+        $typ = "ilustrace";
+        break;
+  }
+
+  $otazky = $database->select("Otazky", ["id"], ["type" => $typ]);
+  print_r($otazky);
+  $keys = array_rand($otazky, 3);
+  print_r($keys);
+
+  $database->delete("GamesOtazky", ["game_id" => $gameCode]);
+
+  $order = 0;
+  foreach ($keys as $key) {
+    $database->insert("GamesOtazky", [
+      "game_id" => $_GET['code'],
+      "otazka_id" => $otazky[$key]["id"],
+      "position" => $order,
+    ]);
+    $order++;
+  }
+  exit();
 ?>
