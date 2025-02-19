e63f5ba062ca8ed9f61a79b1b3a5622ba8203b47
Samuel Huf <samhuf@seznam.cz>
2025-02-19 12:54:52 +0100

    dasjfsdkfpafpij

--- game.js
+++ game.js
@@ -8,4 +8,15 @@ async function generateQuestions() {
     })
 }
 
+data = {}
+
+async function getQuestions() {
+    const formData = new FormData()
+    formData.append('getJSON', 'true')
+    data = fetch('http://pubz.infinityfreeapp.com/api/get-questions.php', {
+        method: 'POST',
+        body: formData
+    })
+}
+
 generateQuestions()
