a798c1d01e2c555661806c988ff2a9b34f265223
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-19 10:08:03 +0100

    add fetch for game stage

---
+++ api/get-stage.php
@@ -0,0 +1,14 @@
+<?php
+
+require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
+global $database;
+
+$data = $database->select("Players", "name", [
+	"game" => $_GET["game"]
+]);
+
+foreach($data as $item) {
+	echo $item . "<br>";
+}
+
+?>
