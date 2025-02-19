6ba1fb98fa67316926a3c2faee304e68e636ec27
Samuel Huf <samhuf@seznam.cz>
2025-02-19 21:47:03 +0100

    asdkjfasdfsldfkj

--- api/get-questions.php
+++ api/get-questions.php
@@ -98,7 +98,7 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["getFinished"])) {
       $players = $database->select("Players", "stage_finished", ["game" => $gameCode]);
-      $jsonResponse = ["allFinished" => !in_array("0", $players)]
+      $jsonResponse = ["allFinished" => !in_array("0", $players)];
       echo json_encode($jsonResponse);
       exit();
     }
