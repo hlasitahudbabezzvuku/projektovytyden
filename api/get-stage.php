613a49d581614952e6236ab2eaf1cfa5a1c9b6cc
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-19 10:46:05 +0100

    make get-stage.php

--- api/get-stage.php
+++ api/get-stage.php
@@ -3,8 +3,8 @@
 require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
 global $database;
 
-$data = $database->select("Players", "name", [
-	"game" => $_GET["game"]
+$data = $database->select("Games", "stage", [
+	"id" => $_GET["game"]
 ]);
 
 foreach($data as $item) {
