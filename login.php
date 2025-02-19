1e8f6b86cdff017a044abd501aaf7a0fa9217fff
Samuel Huf <samhuf@seznam.cz>
2025-02-19 19:18:04 +0100

    lkjlkjljlkjlkjlkj

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ global $database;
 
       <img src="https://api.qrserver.com/v1/create-qr-code/?data=<?php echo urlencode('http://pubz.infinityfreeapp.com/login.php?game=' . $code) ?>" alt="qr-code"/>
       <form action="monitor.php" method="GET">
-      <input type="hidden" name='id' value=<?php $code ?>>
+      <input type="hidden" name='id' value=<?php echo $code ?>>
       <?php echo $code ?>
       <div id="players"></div>
       <script>
--- monitor.php
+++ monitor.php
@@ -1,24 +1,32 @@
 <?php
-    session_start();
+  require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
+  session_start();
 
-    if (array_key_exists("id", $_GET)) {
-        $_SESSION['code'] = $_GET["id"];
-    } else {
-        header("Location: http://pubz.infinityfreeapp.com/index.php?failed=" . urlencode("Prvni musis hru zalozit :("));
-    }    
+  if (array_key_exists("id", $_GET)) {
+      $_SESSION['code'] = $_GET["id"];
+  } else {
+      header("Location: http://pubz.infinityfreeapp.com/index.php?failed=" . urlencode("Prvni musis hru zalozit :("));
+  } 
+  
+  function addStage() {
+    $gameCode = $_SESSION['code']
+    $currentStage = $database->get("Games", 'stage', [
+      "id" => $gameCode
+    ]);
+    $database->update("Games", ['stage[+]' => 1], ["id" => $gameCode]);
+  }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
-<>
-    <script src="game.js"></script>
-    <?php if ($_SERVER['REQUEST_METHOD'] == "GET") { ?>
-      <script>generateQuestions()</script>
-    <?php } ?>
+  <script src="game.js"></script>
+  <?php if ($_SERVER['REQUEST_METHOD'] == "GET") { ?>
+    <script>generateQuestions()</script>
+  <?php } ?>
 </body>
 </html>
