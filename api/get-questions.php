1345a8e71709cd27693446a0cd163534b86441ba
Samuel Huf <samhuf@seznam.cz>
2025-02-19 10:04:35 +0100

    zxcvbnm

--- api/get-questions.php
+++ api/get-questions.php
@@ -1,8 +1,4 @@
 <?php
-    if (isset($_SESSION) || session_status() !== PHP_SESSION_NONE) {
-        session_destroy();
-    }
-
     session_start();
     require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
 
