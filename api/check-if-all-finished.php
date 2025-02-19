c993629899bc7fbc9940977779dc15e96dde9016
Samuel Huf <samhuf@seznam.cz>
2025-02-20 00:19:27 +0100

    poiusadpaiuosd

--- api/check-if-all-finished.php
+++ api/check-if-all-finished.php
@@ -4,10 +4,8 @@
 
   $gameCode = $_GET['code'];
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["getFinished"])) {
-    $players = $database->select("Players", "stage_finished", ["game" => $gameCode]);
-    $jsonResponse = ["allFinished" => !in_array("0", $players)];
-    echo json_encode($jsonResponse);
-    exit();
-  }
+  $players = $database->select("Players", "stage_finished", ["game" => $gameCode]);
+  $jsonResponse = ["allFinished" => !in_array("0", $players)];
+  echo json_encode($jsonResponse);
+  exit();
 ?>
