37c15ecd1956916487b36763dadf9cb95cd9a558
Samuel Huf <samhuf@seznam.cz>
2025-02-19 09:23:13 +0100

    pridani get-questions.php

---
+++ api/get-questions.php
@@ -0,0 +1,20 @@
+<?php
+    if (isset($_SESSION) || session_status() !== PHP_SESSION_NONE) {
+        session_destroy();
+    }
+
+    session_start();
+    require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
+
+    $gameCode = $_SESSION["code"];
+
+    $currentStage = $database->get("Games", null, 'stage');
+    $textOtazky = $database->select("textOtazky", ["[>]Odpovedi"=>["id_odpovedi" => "id"]], ["text", "a", "b", "c", "d", "spravna"]);
+    
+    for ($i = 0; $i < count($textOtazky); $i++) {
+        $textOtazky[$i]["id"] = bin2hex($textOtazky[$i]["id"]);
+        $textOtazky[$i]["id_odpovedi"] = bin2hex($textOtazky[$i]["id_odpovedi"]);
+    }
+    $jsonOtazky = json_encode($textOtazky);
+    print_r($currentStage);
+?>
--- game.php
+++ game.php
@@ -5,13 +5,4 @@
 
     session_start();
     require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
-
-    $textOtazky = $database->select("textOtazky", ["[>]Odpovedi"=>["id_odpovedi" => "id"]], ["text", "a", "b", "c", "d", "spravna"]);
-
-    for ($i = 0; $i < count($textOtazky); $i++) {
-        $textOtazky[$i]["id"] = bin2hex($textOtazky[$i]["id"]);
-        $textOtazky[$i]["id_odpovedi"] = bin2hex($textOtazky[$i]["id_odpovedi"]);
-    }
-    // $jsonOtazky = json_encode($textOtazky);
-    print_r($textOtazky);
 ?>
