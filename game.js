f317bfa2382a55062fff4bc65eff6452ff02895e
Samuel Huf <samhuf@seznam.cz>
2025-02-19 13:03:01 +0100

    asdasasdsa

--- game.js
+++ game.js
@@ -17,11 +17,15 @@ async function getQuestions() {
         method: 'POST',
         body: formData
     })
-        .then((response) => {
-            data = response
+        .then((response) => response.json()) // Parse the JSON response
+        .then((data) => {
+            // Store the response in a variable
+            const responseData = data
+            console.log(responseData) // This will log the response to the console
+            // You can now use responseData as needed in your application
         })
         .catch((error) => {
-            console.log(error)
+            console.error('Error:', error) // Handle any errors
         })
 }
 
