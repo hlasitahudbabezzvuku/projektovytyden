3416344e8b0be5ca765e6504fbbb7ff859832c09
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-19 21:57:27 +0100

    move error handling into body in index.php

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ if (isset($_SESSION) || session_status() !== PHP_SESSION_NONE) {
   session_destroy();
 }
 
-if (array_key_exists("failed", $_GET)) {
-  echo("<span style=\"color: #ff0000;\">" . $_GET["failed"] . "</span>");
-}
-
 require $_SERVER["DOCUMENT_ROOT"] . "/utils/database.php";
 global $database;
 
@@ -21,6 +17,13 @@ global $database;
   </head>
   <body>
     <h1>PubZ</h1>
+
+    <?php
+    if (array_key_exists("failed", $_GET)) {
+      echo("<span style=\"color: #ff0000;\">" . $_GET["failed"] . "</span>");
+    }
+    ?>
+
     <span><a href="login.php?mode=join">Připojit se</a></span>
     <span><a href="login.php?mode=host">Vytvořit</a></span>
     <span><a href="login.php?mode=single">Hrát sám</a></span>
