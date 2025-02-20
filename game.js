8d8da290a4c73a1e668feecccc033f58b9a85641
Samuel Huf <samhuf@seznam.cz>
2025-02-20 21:44:52 +0100

    dsapfoiu

--- game.js
+++ game.js
@@ -238,7 +238,9 @@ async function loadQuestion(gameCode, playerId) {
       if (categories[currentCategoryIndex] === 'Zvuk') {
         mediaPlaceholder.style.height = '80px'
         mediaPlaceholder.innerHTML =
-          '<audio width="100%" heigh="50%" id="myAudio" src="path/to/your/audio-file.mp3" preload="auto"></audio><button onclick="playAudio()">Play Audio</button><button onclick="pauseAudio()">Pause Audio</button>'
+          '<audio width="100%" heigh="50%" id="myAudio" src="/home/vol15_3/infinityfree.com/if0_38314982/' +
+          questions[currentQuestionIndex].zvuk +
+          '" preload="auto"></audio><button onclick="playAudio()">Play Audio</button><button onclick="pauseAudio()">Pause Audio</button>'
       } else if (categories[currentCategoryIndex] === 'Video') {
         mediaPlaceholder.style.height = '300px'
         mediaPlaceholder.innerHTML =
