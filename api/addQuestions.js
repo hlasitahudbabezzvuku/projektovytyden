99f22a5e54562f7bccdd004f2a1d9a69b3f628c9
Samuel Huf <samhuf@seznam.cz>
2025-02-20 18:51:12 +0100

    dafjskdfpiu

--- api/addQuestions.js
+++ api/addQuestions.js
@@ -1,125 +1,206 @@
 const otazky = {
   textove: {
     0: {
-      text: 'What is the capital of France?',
+      text: 'Jaké jsou dvě nejznámější zpěvačky naší generace?',
       odpovedi: {
-        a: 'Berlin',
-        b: 'Madrid',
-        c: 'Paris',
-        d: 'Rome',
-        spravna: 'c'
-      }
+        a: 'Taylor Swift a Billie Eilish',
+        b: 'Madona a Lady Gaga',
+        c: 'Sabrina Carpenter a Taylor Swift',
+        d: 'Billie Eilish a Lana Del Rey',
+        spravna: 'a'
+      },
+      vysvetlivka:
+        'Taylor Swift a Billie Eilish jsou dvě zpěvačky, které se neustále objevují na top žebříčcích Spotify.'
     },
     1: {
-      text: 'What is 2 + 2?',
+      text: 'Jaký hudební žánr je na 2. místě nejposlouchanějších žánrů hudby generace Z?',
       odpovedi: {
-        a: '3',
-        b: '4',
-        c: '5',
-        d: '6',
-        spravna: 'b'
-      }
+        a: 'Hip hop',
+        b: 'Pop',
+        c: 'Rap',
+        d: 'Metal',
+        spravna: 'c'
+      },
+      vysvetlivka:
+        'Rap je žánr hudby, který inspiruje generaci Z nejen jeho hudebností, ale i stylem oblékání rapperů.'
     },
     2: {
-      text: "Who wrote '1984'?",
+      text: 'Jak se nazývá elektronická cigareta s příchutí?',
       odpovedi: {
-        a: 'George Orwell',
-        b: 'J.K. Rowling',
-        c: 'Aldous Huxley',
-        d: 'F. Scott Fitzgerald',
-        spravna: 'a'
+        a: 'Shisha pen',
+        b: 'Vapo',
+        c: 'Nikotinová trubička',
+        d: 'Smoke stick',
+        spravna: 'b'
       }
     },
     3: {
-      text: 'What is the largest planet in our solar system?',
+      text: 'Co znamená slovo rizz?',
       odpovedi: {
-        a: 'Earth',
-        b: 'Jupiter',
-        c: 'Saturn',
-        d: 'Mars',
-        spravna: 'b'
+        a: 'Schopnost někoho svést a umění flirtovat',
+        b: 'Řezat',
+        c: "Zkratka pro 'ryzí zlato'",
+        d: 'Zkratka pro rychlou zprávu',
+        spravna: 'a'
       }
     },
     4: {
-      text: 'What is the chemical symbol for water?',
+      text: 'Co znamená slovo gyatt?',
       odpovedi: {
-        a: 'H2O',
-        b: 'CO2',
-        c: 'O2',
-        d: 'H2',
+        a: "Zkratka pro 'Get your attention today'",
+        b: 'Fitness trend na platformě TikTok',
+        c: 'Výraz údivu / atraktivní hýždě',
+        d: 'Slangový výraz pro drahé oblečení',
+        spravna: 'c'
+      }
+    },
+    5: {
+      text: "Pod kterého zahraničního kontroverzního rappera spadá značka oblečení s názvem 'Yeezy'?",
+      odpovedi: {
+        a: 'Drake',
+        b: 'Kendrick Lamar',
+        c: 'Kanye West',
+        d: 'Asap Rocky',
+        spravna: 'c'
+      }
+    },
+    6: {
+      text: 'Co znamená pojem catfish?',
+      odpovedi: {
+        a: 'Ryba, která se vyskytuje ve sladkých vodách',
+        b: 'Osoba, která vytváří falešnou online identitu za účelem podvodu',
+        c: 'Osoba, která se věnuje rybaření',
+        d: 'Slang pro někoho, kdo nejí ryby',
+        spravna: 'b'
+      }
+    },
+    7: {
+      text: 'Jaký člověk je označovaný pod pojmem fuckboy?',
+      odpovedi: {
+        a: 'Někdo, kdo je vždy ochotný pomoct',
+        b: 'Osoba, která ráda pohybuje v přírodě',
+        c: 'Muž, který má zájem pouze o povrchní vztahy',
+        d: 'Někdo, kdo se provokativně obléká',
+        spravna: 'c'
+      }
+    },
+    8: {
+      text: 'Který kus oblečení je v generaci Z nejvíce nenáviděný?',
+      odpovedi: {
+        a: 'Úzké džíny',
+        b: 'Volné džíny',
+        c: 'Zvonáče',
+        d: 'Uplé trička',
         spravna: 'a'
+      }
+    },
+    9: {
+      text: 'Co znamená slovo vibe?',
+      odpovedi: {
+        a: 'Místo pro relaxaci a odpočinek',
+        b: 'Atmosféra nebo pocit, který vyvolává kladné emoce',
+        c: 'Speciální druh tanečního pohybu',
+        d: 'Hudební elektronický nástroj',
+        spravna: 'b'
+      }
+    },
+    10: {
+      text: 'Co znamená výraz hoe?',
+      odpovedi: {
+        a: 'Osoba, která se věnuje zahradničení',
+        b: 'Dívka zaměřující se na akademické úspěchy',
+        c: 'Kolegyně ve fitness komunitě',
+        d: 'Dívka, která se ve vztazích chová promiskuitně nebo nezávazně',
+        spravna: 'd'
+      }
+    },
+    11: {
+      text: 'Co znamená slovo cringe?',
+      odpovedi: {
+        a: 'Něco super cool a trendy',
+        b: 'Trapné',
+        c: 'Vtipná věc',
+        d: 'Slangový výraz pro pohodu',
+        spravna: 'b'
       }
     }
   },
   video: {
     0: {
       video: 'https://youtu.be/Bbe9D4_D80M?si=neE0MLUueFMAyc6q',
-      odpoved: 'https://youtu.be/QvVShAERPxw?si=FzAJVRfW78BionD-',
-      otazka: 'Dokonči větu: Jožo, ty prestaň klamať...',
+      text: 'Dokonči větu: Jožo, ty prestaň klamať...',
       odpovedi: {
         a: 'tomu ujovi',
         b: 'do reportáže',
         c: 'do telky',
         d: 'dobre?',
         spravna: 'c'
-      }
+      },
+      vysvetlivka: 'Kulturní reference na známou reportáž generace Z.'
     },
     1: {
       video: 'https://www.youtube.com/watch?v=OSJ-YC4LEL8',
-      otazka: 'Co dělá člověk na videu?',
+      text: 'Co dělá člověk na videu?',
       odpovedi: {
         a: 'bělí si zuby',
         b: 'nabíjí si',
         c: 'dává si žvýkačku',
         d: 'fetuje',
         spravna: 'b'
-      }
+      },
+      vysvetlivka:
+        '„Nabíjí si” - slang pro aplikování nikotinového sáčku pod ret.'
     },
     2: {
       video: 'https://www.youtube.com/shorts/4FXpfLDhvuo',
-      odpoved: 'https://www.youtube.com/shorts/yJOTx4rVOzo',
-      otazka: 'Jak se jmenuje interpret na videu?',
+      text: 'Jak se jmenuje interpret na videu?',
       odpovedi: {
         a: 'Sergei B',
         b: 'Viktor S',
         c: 'Calin',
         d: 'Yzomandias',
         spravna: 'a'
-      }
+      },
+      vysvetlivka:
+        'Je to rapper známý svými expresivními výlevy na sociálních sítích.'
     },
     3: {
       video: 'https://www.youtube.com/watch?v=6V7_LNhqI1k',
-      odpoved: 'https://www.youtube.com/watch?v=ZicUPyY14xA',
-      otazka: 'Pro koho video točí?',
+      text: 'Pro koho video točí?',
       odpovedi: {
         a: 'pro babičku',
         b: 'pro manžela',
         c: 'pro pojišťovnu',
         d: 'pro spořku',
         spravna: 'c'
-      }
+      },
+      vysvetlivka: 'Napodobení virálního videa z TikToku.'
     },
     4: {
       video: 'https://youtu.be/C4FOiXdDpJ8?si=BJGRWO4vtinGFyzW',
-      otazka: 'Jak se nazývá tento typ videa?',
+      text: 'Jak se nazývá tento typ videa?',
       odpovedi: {
         a: 'rozbalovačka',
         b: 'boxing',
         c: 'unboxing',
         d: 'vystavovačka',
         spravna: 'c'
-      }
+      },
+      vysvetlivka:
+        'Unboxing je typ videa, ve kterém se rozbalují nově pořízené předměty.'
     },
     5: {
       video: 'https://www.youtube.com/watch?v=FtXMyOz01sk',
-      otazka: 'Co míchá muž na videu?',
+      text: 'Co míchá muž na videu?',
       odpovedi: {
         a: 'kratom',
         b: 'matcha',
         c: 'kakao',
         d: 'zelený čaj',
         spravna: 'a'
-      }
+      },
+      vysvetlivka:
+        'Kratom je návyková látka ve formě zeleného prášku, který se míchá s nápojem.'
     }
   },
   zvuk: {
