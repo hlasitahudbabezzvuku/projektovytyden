3750a2d3b86172e3824fb33a51251c139606c510
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-20 20:12:10 +0100

    remove statement

--- api/ping.php
+++ api/ping.php
@@ -7,11 +7,7 @@ session_start();
 
 if (isset($_SESSION) && session_status() !== PHP_SESSION_NONE) {
   if(!empty($database->get("Players", "id", [ "id" => $_SESSION["uuid"] ]))) {
-    if (!empty($database->get("Games", "id", [ "id" => $_SESSION["game"] ]))) {
-      $database->update("Players", [ "last_ping" => time() ], [ "id" => $_SESSION["uuid"] ]);
-    } else {
-      echo("Game is no longer avalible");
-    }
+    $database->update("Players", [ "last_ping" => time() ], [ "id" => $_SESSION["uuid"] ]);
   } else {
     echo("Error: Player not found");
   }
