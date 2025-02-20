45cf2bf33cbf701c78be46dfdc4f35f9674e6276
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-20 09:29:26 +0100

    comment fix

--- login.php
+++ login.php
@@ -16,21 +16,21 @@ global $database;
     <title>PubZ</title>
   </head>
 
-  <body class="min-h-screen flex items-center justify-center p-4">
-    <div class="w-full max-w-lg p-6 fade-in flex flex-col justify-center">
-      <a href="index.html" class="arrow-button self-start mb-4">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>
-        </svg>
-      </a>
-      <h2 class="text-2xl font-bold mb-4 text-center">Online Match</h2>
-      <label for="onlinePlayerName" class="self-start mb-2">Zadej své jméno</label>
-      <input type="text" id="onlinePlayerName" placeholder="Tvé jméno" class="mb-4 p-3 rounded bg-gray-800 text-white placeholder-gray-400 w-full"/>
-      <label for="gameCode" class="self-start mb-2">Zadej kód hry</label>
-      <input type="text" id="gameCode" placeholder="Kód hry" class="mb-4 p-3 rounded bg-gray-800 text-white placeholder-gray-400 w-full"/>
-      <a id="onlineJoinBtn" href="#" class="button w-full">Připojit se</a>
-    </div>
-  </body>
+  <!--<body class="min-h-screen flex items-center justify-center p-4">-->
+  <!--  <div class="w-full max-w-lg p-6 fade-in flex flex-col justify-center">-->
+  <!--    <a href="index.html" class="arrow-button self-start mb-4">-->
+  <!--      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">-->
+  <!--        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"/>-->
+  <!--      </svg>-->
+  <!--    </a>-->
+  <!--    <h2 class="text-2xl font-bold mb-4 text-center">Online Match</h2>-->
+  <!--    <label for="onlinePlayerName" class="self-start mb-2">Zadej své jméno</label>-->
+  <!--    <input type="text" id="onlinePlayerName" placeholder="Tvé jméno" class="mb-4 p-3 rounded bg-gray-800 text-white placeholder-gray-400 w-full"/>-->
+  <!--    <label for="gameCode" class="self-start mb-2">Zadej kód hry</label>-->
+  <!--    <input type="text" id="gameCode" placeholder="Kód hry" class="mb-4 p-3 rounded bg-gray-800 text-white placeholder-gray-400 w-full"/>-->
+  <!--    <a id="onlineJoinBtn" href="#" class="button w-full">Připojit se</a>-->
+  <!--  </div>-->
+  <!--</body>-->
 
   <body>
     <?php if (array_key_exists("failed", $_GET)) { ?>
