119db89a86b1bfba71b9aa6b6602c0817178705e
Samuel Huf <samhuf@seznam.cz>
2025-02-20 22:00:46 +0100

    asdfsd

--- game.js
+++ game.js
@@ -240,6 +240,7 @@ async function loadQuestion(gameCode, playerId, home) {
         mediaPlaceholder.innerHTML =
           '<audio width="100%" heigh="50%" id="myAudio" src="' +
           home +
+          '/' +
           questions[currentQuestionIndex].zvuk +
           '" preload="auto"></audio><button onclick="playAudio()">Play Audio</button><button onclick="pauseAudio()">Pause Audio</button>'
       } else if (categories[currentCategoryIndex] === 'Video') {
