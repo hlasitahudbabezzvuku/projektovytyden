ba96dd3e880cb90b1365f393b7f8b10fb2eeb67b
Samuel Huf <samhuf@seznam.cz>
2025-02-20 01:37:24 +0100

    sadapoiu

--- stage-end.php
+++ stage-end.php
@@ -10,8 +10,18 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <p>Wait for host to start the game</p>
+
   <script src="game.js"></script>
-  
+  <script>setInterval(() => {
+    fetch('http://pubz.infinityfreeapp.com/api/get-stage.php?game=' + <?php echo $_SESSION["code"] ?>)
+      .then(function (response) { return response.text(); })
+      .then(function (text) {
+        console.log(text);
+        // if (text != '0')
+        //   window.location.replace('http://pubz.infinityfreeapp.com/game.php');
+      });
+  }, 2000)</script>
+
 </body>
 </html>
