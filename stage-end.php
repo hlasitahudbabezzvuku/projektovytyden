3ba89b3666864dbea4d63ebee1213c9f46832f27
Samuel Huf <samhuf@seznam.cz>
2025-02-21 09:51:32 +0100

    aspdofiu

--- stage-end.php
+++ stage-end.php
@@ -49,6 +49,18 @@
           } 
         });
     }, 2000)
+
+    setInterval(() => {
+      fetch('https://pubz.l3dnac3k.net/api/get-stage.php?game=' + '<?php echo $_SESSION["code"]; ?>')
+        .then(function (response) { return response.text(); })
+        .then(function (text) {
+          console.log(text);
+          if (text === '9') {
+            localStorage.removeItem('result')
+            window.location.replace('https://pubz.l3dnac3k.net/end.php');
+          } 
+        });
+    }, 2000)
     
     // frantovo - nesahat!
     function ping() { fetch('https://pubz.l3dnac3k.net/api/ping-player.php')
