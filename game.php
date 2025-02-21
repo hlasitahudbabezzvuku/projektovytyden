3d77833c664a958c0e0942cca9614a01cf937230
hlasitahudbabezzvuku <frantisek.lednicky@outlook.com>
2025-02-21 01:23:03 +0100

    fix game stylesheet loading

--- game.php
+++ game.php
@@ -11,6 +11,7 @@
     <title>PubZ Multiplayer</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"/>
+    <link href="css/game.css" rel="stylesheet"/>
   </head>
   <body class="min-h-screen flex items-center justify-center p-4 bg-black">
     <div class="w-full max-w-lg p-6 flex flex-col space-y-6">
